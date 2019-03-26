d549683a8d3d5dec3e116ec209056820ea2b33ee
Jon Skeet <jonskeet@google.com>
2019-03-26 15:25:15 +0000

    Implement IXmlRepository storing documents in Google Cloud Storage
    This has no *unit* tests as the integration tests are much more useful. Suggestions for unit tests welcome, of course.
    
    It's not clear how ASP.NET Core supports the case where multiple servers add an element for the same purpose at the same time, but that's not something this code can affect.

---
+++ Google.Cloud.AspNetCore/Google.Cloud.AspNetCore.DataProtection.Storage/GoogleCloudDataProtectionBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿// Copyright 2019 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Cloud.AspNetCore.DataProtection.Storage;
+using Google.Cloud.Storage.V1;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.DataProtection
+{
+    /// <summary>
+    /// Contains extension methods for modifying a <see cref="IDataProtectionBuilder"/> to work with Google Cloud Storage.
+    /// </summary>
+    public static class GoogleCloudDataProtectionBuilderExtensions
+    {
+        /// <summary>
+        /// Configures the data protection system to persist keys in Google Cloud Storage.
+        /// </summary>
+        /// <param name="builder">The data protection builder to configure.</param>
+        /// <param name="bucketName">The name of the bucket in which to store the object containing the keys.</param>
+        /// <param name="objectName">The name of the object in which to store the keys.</param>
+        /// <returns>The same builder, for chaining purposes.</returns>
+        public static IDataProtectionBuilder PersistKeysToGoogleCloudStorage(
+            this IDataProtectionBuilder builder, string bucketName, string objectName) =>
+            PersistKeysToGoogleCloudStorage(builder, GetClient(builder), bucketName, objectName);
+
+        private static StorageClient GetClient(IDataProtectionBuilder builder)
+        {
+            // TODO: Use DI to find a StorageClient, or credentials if we can. It's not clear how we can
+            // resolve existing configured dependencies.
+            return StorageClient.Create();            
+        }
+
+        /// <summary>
+        /// Configures the data protection system to persist keys in Google Cloud Storage.
+        /// </summary>
+        /// <param name="builder">The data protection builder to configure.</param>
+        /// <param name="client">The Google Cloud Storage client to use for network requests.</param>
+        /// <param name="bucketName">The name of the bucket in which to store the object containing the keys.</param>
+        /// <param name="objectName">The name of the object in which to store the keys.</param>
+        /// <returns>The same builder, for chaining purposes.</returns>
+        public static IDataProtectionBuilder PersistKeysToGoogleCloudStorage(
+            this IDataProtectionBuilder builder, StorageClient client, string bucketName, string objectName)
+        {
+            builder.Services.Configure<KeyManagementOptions>(options =>
+                options.XmlRepository = new CloudStorageXmlRepository(client, bucketName, objectName));
+            return builder;
+        }
+    }
+}
---
+++ Google.Cloud.AspNetCore/Google.Cloud.AspNetCore.DataProtection.Storage/RetryHandler.cs
@@ -0,0 +1,74 @@
+﻿// Copyright 2019 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+
+namespace Google.Cloud.AspNetCore.DataProtection.Storage
+{
+    /// <summary>
+    /// Simple means of executing retry with exponential backoff and proportional jitter,
+    /// retrying on any GoogleApiException.
+    /// </summary>
+    internal sealed class RetryHandler
+    {
+        private readonly int _maxAttempts;
+        private readonly TimeSpan _initialBackoff;
+        private readonly double _backoffMultiplier;
+        private readonly TimeSpan _maxBackoff;
+        private readonly object _lock = new object();
+        private readonly Random _random = new Random(); // Use for backoff.
+
+        internal RetryHandler(int maxAttempts, TimeSpan initialBackoff, double backoffMultiplier, TimeSpan maxBackoff)
+        {
+            _maxAttempts = maxAttempts;
+            _initialBackoff = initialBackoff;
+            _backoffMultiplier = backoffMultiplier;
+            _maxBackoff = maxBackoff;
+        }
+
+        /// <summary>
+        /// Applies simple retry until the given function returns successfully (or a non-GoogleApiException is thrown).
+        /// </summary>
+        internal T ExecuteWithRetry<T>(Func<T> func)
+        {
+            int attempt = 0;
+            TimeSpan nextBackoff = _initialBackoff;
+            while (true)
+            {
+                try
+                {
+                    return func();
+                }
+                catch (GoogleApiException) when (attempt < _maxAttempts)
+                {
+                    attempt++;
+                    int millisecondsToSleep;
+                    lock (_lock)
+                    {
+                        int nextBackoffMillis = (int)nextBackoff.TotalMilliseconds;
+                        // Apply jitter to the backoff, but only within the range of 50%-100% of the "theoretical" backoff.
+                        millisecondsToSleep = nextBackoffMillis / 2 + _random.Next(nextBackoffMillis / 2);
+                    }
+                    Thread.Sleep(millisecondsToSleep);
+                    nextBackoff = TimeSpan.FromSeconds(nextBackoff.TotalSeconds * _backoffMultiplier);
+                    if (nextBackoff > _maxBackoff)
+                    {
+                        nextBackoff = _maxBackoff;
+                    }
+                }
+            }
+        }
+    }
+}
